3bda4a4118be46144fee0791880056c280a8b464
Richard Levenberg <richardl@enterprise.ufp.com>
2011-03-24 11:48:15 -0700

    allow external user details

--- src/main/java/com/ufp/security/identity/authentication/IdentityAuthenticationProvider.java
+++ src/main/java/com/ufp/security/identity/authentication/IdentityAuthenticationProvider.java
@@ -1,23 +1,43 @@
 package com.ufp.security.identity.authentication;
 
+import org.springframework.beans.factory.InitializingBean;
+
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.util.Assert;
 
 import com.ufp.security.identity.core.ContinueAuthenticationException;
 import com.ufp.security.identity.core.DisplayItem;
 
-import org.apache.log4j.Logger;
+public class IdentityAuthenticationProvider implements AuthenticationProvider, InitializingBean {
+    private UserDetailsService userDetailsService;
 
-public class IdentityAuthenticationProvider implements AuthenticationProvider {
-    private static Logger logger = Logger.getLogger(IdentityAuthenticationProvider.class);
+    public void afterPropertiesSet() throws Exception {
+        Assert.notNull(this.userDetailsService, "The userDetailsService must be set");
+    }
 
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        logger.debug("processing " + authentication);
-        return authentication;
+        Authentication newAuthentication = null;
+        if (authentication instanceof IdentityAuthenticationToken) {
+            IdentityAuthenticationToken token = (IdentityAuthenticationToken)authentication;
+            UserDetails userDetails = userDetailsService.loadUserByUsername(token.getPrincipal().toString());
+            newAuthentication = createSuccessfulAuthentication(userDetails, token);
+        }
+        return newAuthentication;
+    }
+
+    protected Authentication createSuccessfulAuthentication(UserDetails userDetails, IdentityAuthenticationToken authentication) {
+        return new IdentityAuthenticationToken(userDetails, userDetails.getAuthorities());
     }
 
     public boolean supports(Class<? extends Object> authentication) {
         return (IdentityAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+
+    public void setUserDetailsService(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
     }
 }    
