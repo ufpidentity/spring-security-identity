9c03a4e62893c08e7f91c151d21c206e4ba804ff
Richard Levenberg <richardl@ufp.com>
2011-04-08 19:47:30 -0700

    javadoc updated to real examples

--- src/main/java/com/ufp/security/identity/service/Identity4JService.java
+++ src/main/java/com/ufp/security/identity/service/Identity4JService.java
@@ -15,34 +15,39 @@ import com.ufp.security.identity.provider.data.DisplayItem;
  * An example of a failure:
  * <p>
  * <pre>
- * &lt;?xml version="1.0"?&gt;
- * &lt;authentication_pretext&gt;
- *  &lt;result quality="0.0" level="0" message="User not found"&gt;FAILURE&lt;/result&gt;
- * &lt;/authentication_pretext&gt;
+&lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot; standalone=&quot;yes&quot;?&gt;
+&lt;authentication_pretext&gt;
+  &lt;result quality=&quot;0.0&quot; level=&quot;0&quot; message=&quot;User not found&quot;&gt;FAILURE&lt;/result&gt;
+&lt;/authentication_pretext&gt;
  * </pre>
  * <p>
  * An example of a pretext (may contain multiple display items and subsequent calls may return any number of pretexts (n.b. the <code>form_element</code> values are escaped so that they can be directly injected into a page for rendering)
  * <p>
  * <pre>
- * &lt;?xml version="1.0"?&gt;
- * &lt;authentication_pretext&gt;
- *  &lt;result quality="0.0" level="3" message="OK"&gt;SUCCESS&lt;/result&gt;
- *  &lt;display_item name="secret"&gt;
- *    &lt;display_name&gt;Enter Secret&lt;/display_name&gt;
- *    &lt;form_element&gt;&amp;lt;input id="AuthParam0"  type="text" name="secret" /&amp;gt;&lt;/form_element&gt;
- *    &lt;nickname&gt;saw (w/AIM)&lt;/nickname&gt;
- *  &lt;/display_item&gt;
- * &lt;/authentication_pretext&gt;
+&lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot; standalone=&quot;yes&quot;?&gt;
+&lt;authentication_pretext&gt;
+  &lt;result quality=&quot;0.0&quot; level=&quot;3&quot; message=&quot;OK&quot;&gt;SUCCESS&lt;/result&gt;
+  &lt;display_item name=&quot;secret&quot;&gt;
+    &lt;display_name&gt;Enter Secret&lt;/display_name&gt;
+    &lt;form_element&gt;&amp;lt;input id=&amp;quot;AuthParam0&amp;quot; type=&amp;quot;text&amp;quot; name=&amp;quot;secret&amp;quot; /&amp;gt;&lt;/form_element&gt;
+    &lt;nickname&gt;saw (w/AIM)&lt;/nickname&gt;
+  &lt;/display_item&gt;
+  &lt;display_item name=&quot;passphrase&quot;&gt;
+    &lt;display_name&gt;Enter Passphrase&lt;/display_name&gt;
+    &lt;form_element&gt;&amp;lt;input id=&amp;quot;AuthParam1&amp;quot; type=&amp;quot;text&amp;quot; name=&amp;quot;passphrase&amp;quot; /&amp;gt;&lt;/form_element&gt;
+    &lt;nickname&gt;Default Password&lt;/nickname&gt;
+  &lt;/display_item&gt;
+&lt;/authentication_pretext&gt;
  * </pre>
  * <p>
  * An example of a successful authentication
  * <p>
  * <pre>
- * &lt;?xml version="1.0"?&gt;
- * &lt;authentication_context&gt;
- *  &lt;result quality="0.0" level="1" message="OK"&gt;SUCCESS&lt;/result&gt;
- *  &lt;name&gt;alice@example.com&lt;/name&gt;
- * &lt;/authentication_context&gt;
+&lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot; standalone=&quot;yes&quot;?&gt;
+&lt;authentication_context&gt;
+  &lt;result quality=&quot;0.0&quot; level=&quot;1&quot; message=&quot;OK&quot;&gt;SUCCESS&lt;/result&gt;
+  &lt;name&gt;richardl@ufp.com&lt;/name&gt;
+&lt;/authentication_context&gt;
  * </pre>
  */
 
